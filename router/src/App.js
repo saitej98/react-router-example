8257b6045fe95725a5675d330fa5cbe71962a3a3
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-10-02 09:56:09 +0530

    small changes

--- router/src/App.js
+++ router/src/App.js
@@ -7,12 +7,30 @@ import PageNotFound from "./Components/PageNotFound";
 
 function App() {
   return (
+    <div>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+            <li>
+              <Link to="/other">Other</Link>
+            </li>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
--- router/src/Components/Home.jsx
+++ router/src/Components/Home.jsx
@@ -5,22 +5,6 @@ function Home() {
   return (
     <div>
       <h1>Learn React Router Component...</h1>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/cart">Cart</Link>
-            </li>
-            <li>
-              <Link to="/other">Other</Link>
-            </li>
-          </li>
-        </ul>
-      </div>
     </div>
   );
 }
